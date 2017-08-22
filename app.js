9d7b3d26e756a5cf66ab4f80a8684c3f02960b8a
Imran Panjwani <imranpanjwani00@gmail.com>
2017-08-23 01:17:04 +0530

    add carousel

--- app.js
+++ app.js
@@ -1,27 +1,19 @@
 $(document).ready(function(){
-     $('#home').show();
-     $('#myskills').hide();
-     $('#contactme').hide();
+     $('#my-profile-slider').show();
+     $('#contact-me').hide();
  
-   $('#brand-logo').click(function(e){
-     e.preventDefault();
-     $('#home').show();
-     $('#myskills').hide();
-     $('#contactme').hide();
-     
-}) 
-   $('#myskills-button').click(function(e){
-     e.preventDefault();
-     $('#home').hide();
-     $('#myskills').show();
-     $('#contactme').hide();
-})
-    
-   $('#contactme-button').click(function(e){
-     e.preventDefault();
-     $('#home').hide();
-     $('#myskills').hide();
-     $('#contactme').show();
-})
+     $('#brand-logo').click(function(e){
+      e.preventDefault();
+      $('#my-profile-slider').show();
+      $('#contact-me').hide();
+      
+    }) 
+
+    $('#contact-me-button').click(function(e){
+      e.preventDefault();
+      $('#my-profile-slider').hide();
+      $('#contact-me').show();
+    })
 
 });
+
