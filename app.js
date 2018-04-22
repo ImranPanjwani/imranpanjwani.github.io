a48c3e66363df60e640ceb571dd338d3b09ab5b3
imranp-wal <imran.p@westagilelabs.com>
2018-04-22 23:16:29 +0530

    update portfolio

--- app.js
+++ app.js
@@ -1,21 +1,12 @@
-$(document).ready(function(){
-     $('#my-profile-slider').show();
-     $('#contact-me').hide();
- 
-     $('#brand-logo').click(function(e){
-      e.preventDefault();
-      $('#my-profile-slider').show();
-      $('#contact-me').hide();
-      
+document.addEventListener('DOMContentLoaded', function () {
+  document.querySelectorAll('.column').forEach(function(el){
+    el.addEventListener('click', function() {
+       document.querySelector(`.modal#${el.id}`).classList.toggle('is-active');  
     }) 
-
-    $('#contact-me-button').click(function(e){
-      e.preventDefault();
-      $('#my-profile-slider').hide();
-      $('#contact-me').show();
-      $('.navbar-toggle').click();
-    })
-
-
-});
-
+  });
+  document.querySelectorAll('.modal-close').forEach(function(el) {
+    el.addEventListener('click', function(){
+       el.parentNode.classList.remove('is-active'); 
+    });
+  })
+}); 
